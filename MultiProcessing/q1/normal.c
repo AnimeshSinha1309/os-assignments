38f74bacfc24653ecd390d3494776532580fb82e
Animesh Sinha <animeshsinha1309@gmail.com>
2020-10-17 02:23:12 +0530

    Multiprocessing: Q1 bugs fixed
    Memory sharing issues, bad array indices, etc.

--- MultiProcessing/q1/normal.c
+++ MultiProcessing/q1/normal.c
@@ -8,7 +8,7 @@ void merge_sort_1(int *arr, int l, int r) {
     int arr_l[m - l], arr_r[r - m];
     for (int i = l; i < m; i++) arr_l[i - l] = arr[i];
     for (int i = m; i < r; i++) arr_r[i - m] = arr[i];
-    int i = 0, j = 0, k = 0;
+    int i = 0, j = 0, k = l;
     for (; i < m - l && j < r - m; k++)
         arr[k] = (arr_l[i] < arr_r[j]) ? arr_l[i++] : arr_r[j++];
     for (; i < m - l; i++, k++) arr[k] = arr_l[i];
